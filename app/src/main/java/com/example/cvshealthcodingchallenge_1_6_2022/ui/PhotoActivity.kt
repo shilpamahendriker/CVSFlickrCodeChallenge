e85345c0f095147da5b1f3d43c653d4998a17bcc
shilpa <shilpa.mahendriker@yahoo.com>
2022-06-03 01:32:42 -0700

    Added the logic for recent searches

--- app/src/main/java/com/example/cvshealthcodingchallenge_1_6_2022/ui/PhotoActivity.kt
+++ app/src/main/java/com/example/cvshealthcodingchallenge_1_6_2022/ui/PhotoActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -15,27 +15,21 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.Image
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.window.PopupProperties
-import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModelProvider
 import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
-import coil.size.Scale
-import coil.size.Size
-import com.example.cvshealthcodingchallenge_1_6_2022.R
 import com.example.cvshealthcodingchallenge_1_6_2022.backend.FlickrAPIService
 import com.example.cvshealthcodingchallenge_1_6_2022.backend.Repository
-import com.example.cvsheathcodechallenge.models.FlickrPhotoResponse
 import com.example.cvsheathcodechallenge.models.Items
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class PhotoActivity : ComponentActivity() {
 
@@ -45,7 +39,7 @@ class PhotoActivity : ComponentActivity() {
         val retrofitService = FlickrAPIService.getInstance()
         val repository = Repository(retrofitService!!)
         photoActivityViewModel = ViewModelProvider(this, ViewModelFactory(repository = repository)).get(PhotoActivityViewModel::class.java)
-
+        //createOrSaveSearchListSharedPref(emptyList<String>() as MutableList<String>)
         setContent {
 
             val state: MainViewState by photoActivityViewModel.mainViewStateFlow.collectAsState()
@@ -81,6 +75,48 @@ class PhotoActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun createOrSaveSearchListSharedPref(listSavedInSharedPrefs: MutableList<String>){
+        // Call this inside onCreate to create and pass empty list
+        val convertedData = Gson().toJson(listSavedInSharedPrefs)
+        val sharedPreference =  getSharedPreferences("RECENT_SEARCH_PREF", Context.MODE_PRIVATE)
+        var editor = sharedPreference.edit()
+        editor.putString("KEY",convertedData)
+        editor.putLong("l",100L)
+        editor.commit()
+    }
+
+    private fun retrieveSharedPrefList() : MutableList<String>{
+        val sharedPreference =  getSharedPreferences("RECENT_SEARCH_PREF",Context.MODE_PRIVATE)
+        val gson = Gson()
+        val json = sharedPreference.getString("KEY",null)
+        val type = object : TypeToken<ArrayList<String>>(){}.type//converting the json to list
+        return gson.fromJson(json,type)//returning the list
+    }
+    private fun saveRecentSearch(recentSearch: String) : List<String>{
+
+        val listSavedInSharedPrefs : MutableList<String> = retrieveSharedPrefList()
+
+        // Call this method on Search button click
+        if (listSavedInSharedPrefs.size < 5){
+            listSavedInSharedPrefs.add(0,recentSearch)
+        } else {
+            listSavedInSharedPrefs.removeAt(4)
+            listSavedInSharedPrefs.add(0,recentSearch)
+        }
+        createOrSaveSearchListSharedPref(listSavedInSharedPrefs)
+        return listSavedInSharedPrefs
+    }
+    private fun retrieveRecentSearch(): List<String>{
+        // call this method on focus on Search TextField and use this returned list as drop down
+        val listSavedInSharedPrefs : MutableList<String> = retrieveSharedPrefList()
+        if (listSavedInSharedPrefs.size >= 1){
+            listSavedInSharedPrefs.add(0, "Recent Searches")
+        } else if (listSavedInSharedPrefs.isEmpty()) {
+            listSavedInSharedPrefs.add("You have no recent searches")
+        }
+        return listSavedInSharedPrefs
     }
 }
 
@@ -227,7 +263,7 @@ fun PhotoStateView( photos: List<Items> ){
 // Tried to create an auto complete text view but did not work out nicely need some more time on this
 @Composable
 fun AutoCompleteTextField(photoActivityViewModel : PhotoActivityViewModel){
-    var masterSuggestions = listOf ("porcupine", "tiger", "lion")
+    var masterSuggestions = listOf ("porcupine", "tiger", "lion") // instead of this list pass this function retrieveRecentSearch()
     var suggestions = remember { mutableStateListOf <String>() }
     var selectedText by remember { mutableStateOf("") }
     val context = LocalContext.current
